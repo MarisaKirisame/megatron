5ce9a5b4a91c7a493554d921f5be5b6cb3129d9c
Marisa Kirisame <marisa@cs.utah.edu>
2024-09-24 15:59:57 -0600

    save

--- bin/main.ml
+++ bin/main.ml
@@ -238,6 +238,7 @@ let default_tag : (string, unit) Hashtbl.t =
          "CG-AUTO-PIECES";
          "COORD";
          "COORDS";
+         "PIECE";
        ])
 
 let rec node_to_html_buffer (b : Buffer.t) (parent_x : int) (parent_y : int) (n : _ node) : unit =
