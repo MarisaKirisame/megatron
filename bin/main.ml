8149af1bea5f2a52e6bf3bc982f9a09c7b050ebb
Marisa Kirisame <marisa@cs.utah.edu>
2024-09-26 01:23:03 -0600

    save

--- bin/main.ml
+++ bin/main.ml
@@ -860,9 +860,9 @@ module MainFSI = Main (Megatron.EvalFS.EVAL (S))
 (*module MainFSC = Main (Megatron.EvalFS.EVAL (D))*)
 
 (*module MainDBI = Main (Megatron.EvalDB.EVAL (S))*)
-(*module MainDBC = Main (Megatron.EvalDB.EVAL (D))*)
+module MainDBC = Main (Megatron.EvalDB.EVAL (D))
 
 (*module MainPQI = Main (Megatron.EvalPQ.EVAL (S))*)
-(*module MainPQC = Main (Megatron.EvalPQ.EVAL (D))*)
+module MainPQC = Main (Megatron.EvalPQ.EVAL (D))
 (*module MainHBC = Main (Megatron.EvalHB.EVAL (D))
 *)
