e09e9d4c65118707cc80709b39849af232682335
Wtz_LASR <wtz_lasr@outlook.com>
2024-10-15 13:38:07 +0800

    fix error real this time

--- lib/Compile.ml
+++ lib/Compile.ml
@@ -707,6 +707,7 @@ let compile (prog : prog) defs main meta_defs (ds : destringed list) c : unit =
   List.iter defs ~f:(compile_def prog c);
   Stdio.Out_channel.output_string c "int main(){";
   Stdio.Out_channel.output_string c "tos = new TotalOrderS();";
-  Stdio.Out_channel.output_string c "current_time = &tos->smallest();";
+  Stdio.Out_channel.output_string c "TotalOrderS sm = tos->smallest();";
+  Stdio.Out_channel.output_string c "current_time = &sm;";
   compile_proc c (main |> optimize prog);
   Stdio.Out_channel.output_string c "}"
