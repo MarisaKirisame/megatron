d4f849be6332b2cdac802b83f814b55d11fe6932
霧雨魔理沙 <marisa@cs.utah.edu>
2024-10-17 15:50:44 -0600

    Update header.h

--- include/header.h
+++ include/header.h
@@ -561,8 +561,7 @@ int64_t read_papi() {
 #define barrier() do{rmb();			\
     unsigned tmp;\
     __cpuid(0, tmp, tmp, tmp, tmp);rmb();}while(0)
-#define barrier() do{ unsigned tmp;\
-    __cpuid(0, tmp, tmp, tmp, tmp);}while(0)
+//#define barrier() do{unsigned tmp;__cpuid(0, tmp, tmp, tmp, tmp);}while(0)
 //#define barrier()
 template <typename T>
 static inline T atomic_load(T* t)
